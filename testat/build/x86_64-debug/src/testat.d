009c5f2f54233a5709569ba1bce7b56c5f2e2bf0
tingang <tingangj@hotmail.com>
2017-05-05 07:13:59 +0200

    add a commit

---
+++ testat/build/x86_64-debug/src/testat.d
@@ -0,0 +1,1 @@
+build/x86_64-debug/src/testat.o: src/testat.c
---
+++ testat/src/testat.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Hello World kiki kaka!!!"); /* prints Hello World!!! */
+	return EXIT_SUCCESS;
+}
